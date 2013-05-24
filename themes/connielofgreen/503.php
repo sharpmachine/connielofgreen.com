7ac900c827ec3d76e8b5bb4fd127ea2c7e1f3424
sharpmachine <info@sharpmachinemedia.com>
2013-05-24 12:36:11 -0700

    removing uploads

---
+++ themes/connielofgreen/503.php
@@ -0,0 +1,11 @@
+<table class="datatable" summary="Main Page" align="center" cellpadding="0px" cellspacing="0px">
+<tr height="50px"><td>&nbsp;</td></tr>
+<tr>
+    <th><img src="http://connielofgreen.com/wp-content/themes/connielofgreen/images/logo.png" width="254" height="58" />
+</th>
+</tr>
+<tr height="10px"><td>&nbsp;</td></tr>
+<tr><td align="right"><p style="color:#4d4e53; font-family: Arial, Helvetica, sans-serif">We are currently undergoing routine maintenance.  Please check back soon.</p>
+
+</td></tr> 
+</table>
